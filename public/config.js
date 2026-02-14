3a9d6cce4673e26f75d389cad906024cb355407b
ReyZ4YouXGod <raihanananda767@gmail.com>
2026-02-14 17:43:37 +0700

    Add configuration settings for GitHub and API

---
+++ public/config.js
@@ -0,0 +1,12 @@
+window.CONFIG = {
+    github: {
+        token: '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',
+        owner: 'ReyZ4YouXGod',
+        repo: 'Keamanan',
+        path: 'Dbnomor.json'
+    },
+    api: {
+        loginUrl: '/api/login',
+        userUrl: '/api/users'
+    }
+};
